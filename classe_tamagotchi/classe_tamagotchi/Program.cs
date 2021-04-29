f4d980b43da8fa72384c66e3e88e60661f4bfec2
egili <e.gili.mfp@gmail.com>
2021-04-29 19:34:49 -0300

    novo ex
    primeiro exercicio com criação de classes

---
+++ classe_tamagotchi/classe_tamagotchi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classe_tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
